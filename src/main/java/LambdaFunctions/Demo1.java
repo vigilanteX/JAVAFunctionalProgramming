82b7845615fe5f5fa2d8f11d7b510c295511c971
vigilanteX <dixitaman8@gmail.com>
2025-06-29 20:38:20 +0530

    Old school method of implementing interfaces

---
+++ src/main/java/LambdaFunctions/Demo1.java
@@ -0,0 +1,46 @@
+package LambdaFunctions;
+
+
+/**
+ * Functional interface are those which contains single abstract method, but can
+ * contain n number of static and default methods
+ **/
+interface Shape {
+    void draw();
+
+//    static void m1() {
+//        System.out.println("m1()");
+//    }
+//
+//    static void m2() {
+//        System.out.println("m2()");
+//    }
+}
+
+class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("drawing rectangle...");
+    }
+}
+
+class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("drawing square...");
+    }
+}
+
+public class Demo1 {
+    public static void main(String[] args)
+    {
+        Shape rectangle=new Rectangle();
+        rectangle.draw();
+
+        Shape square=new Square();
+        square.draw();
+
+    }
+}
