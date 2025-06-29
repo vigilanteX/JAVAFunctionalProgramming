ffa6ff195bf5f2f4bc260dd0f7a1bdb0b28f2af0
vigilanteX <dixitaman8@gmail.com>
2025-06-29 22:49:01 +0530

    Implemented calculator using lambda

---
+++ src/main/java/LambdaFunctions/Demo2CreatingCalculatorAppUsingLambda.java
@@ -0,0 +1,29 @@
+package LambdaFunctions;
+
+
+/**
+ * New annotation introduced
+ **/
+@FunctionalInterface
+interface Calculator {
+    int calculate(int a, int b);
+}
+
+public class Demo2CreatingCalculatorAppUsingLambda {
+    public static void main(String[] args)
+    {
+        Calculator sum=(x,y)->x+y;
+        System.out.println(sum.calculate(1,2));
+
+        Calculator subtract=(x,y)->x-y;
+        System.out.println(subtract.calculate(2,1));
+
+        Calculator multiply=(x,y)->x*y;
+        System.out.println(multiply.calculate(2,4));
+
+        Calculator divide=(x,y)->x/y;
+        System.out.println(divide.calculate(8,2));
+
+
+    }
+}
