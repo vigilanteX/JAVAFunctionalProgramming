998eb2cadb600ef10a8ea35e090baa0056f94509
vigilanteX <dixitaman8@gmail.com>
2025-07-09 20:56:46 +0530

    Main method inside interface

---
+++ src/main/java/Java8Interface/StaticMethods/MainMethodInsideInterface.java
@@ -0,0 +1,8 @@
+package Java8Interface.StaticMethods;
+
+public interface MainMethodInsideInterface
+{
+    public static void main(String[] args) {
+        System.out.println("main called");
+    }
+}
